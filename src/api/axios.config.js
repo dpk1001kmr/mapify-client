c66ed7ae2b72dacf9fcc981fa1573674e6e77675
dpk1001kmr <deepak8364kumar@gmail.com>
2025-10-04 11:42:28 +0530

    .

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://mapify-server-23q3.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
